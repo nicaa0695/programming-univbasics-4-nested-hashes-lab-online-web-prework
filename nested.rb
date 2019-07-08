1eaa3d8ba055d690b46b6e0734076fa2574ad287
Veronica Lopez <nicaa0695@gmail.com>
2019-07-08 18:38:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,9 +86,8 @@ def adding_matz
        :known_for => "Ruby",
        :languages => ["LISP, C"]
      }
-
-    
-end
+     
+  end
 
 def changing_alan
 	programmer_hash = 
@@ -108,6 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
      
 end
@@ -128,5 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
 
 end
