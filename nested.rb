311b91920a7368cbea4decca028bad20ff9e347d
Veronica Lopez <nicaa0695@gmail.com>
2019-07-08 18:35:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,9 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
 end
 
-programmer_hash[:alan_kay][:known_for]
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
